4f0e1e351c133a824e7851e4783329ee7a4f02ca
MelkorCC <keanu.kullmann@informatik.hs-fulda.de>
2019-05-13 18:51:25 +0200

    for some reason gitkraken forgot this files

--- CompilerConstruction/src/FirstGrammar/parser/tree/Node.java
+++ CompilerConstruction/src/FirstGrammar/parser/tree/Node.java
@@ -1,33 +1,5 @@
-package FirstGrammar;
-
-import java.util.LinkedList;
-import java.util.List;
+package FirstGrammar.parser.tree;
 
 public abstract class Node {
 	
-}
-
-class RuleNode extends Node {
-	private List<Node> kids;
-	
-	public RuleNode() {
-		super();
-		this.kids = new LinkedList<Node>();
-	}
-	
-	public void append(Node n) {
-		kids.add(n);
-	}
-}
-
-class TokenNode extends Node {
-	private Token token;
-	
-	public TokenNode(Token token) {
-		this.token = token;
-	}
-	
-	public String getText() {
-		return token.getValue();
-	}
 }
