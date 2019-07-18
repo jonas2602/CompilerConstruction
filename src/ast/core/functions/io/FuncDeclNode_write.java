8bb5278b3707948d39b01184e669f2f1d38566dc
Jonas <jonas.schneider96@freenet.de>
2019-07-18 18:21:52 +0200

    merged write and writeln

--- src/ast/core/functions/io/FuncDeclNode_write.java
+++ src/ast/core/functions/io/FuncDeclNode_write.java
@@ -15,30 +15,59 @@ import writer.wrappers.ValueWrapper_Constant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FuncDeclNode_write extends FuncDeclNode_Core {
+public class FuncDeclNode_write extends FuncDeclNode_Core implements Cloneable {
+    private List<AbstractSyntaxTree> m_Elements;
+    private String m_Placeholder;
+    private String m_Ending;
+
     // TODO: maybe use puts instead of printf (no format required)
     public FuncDeclNode_write() {
-        super(IO.WRITE, VoidTypeNode.VoidNode);
+        this(IO.WRITE, "");
+    }
+
+    public FuncDeclNode_write(IO funcName, String ending) {
+        super(funcName, VoidTypeNode.VoidNode);
         m_bCustomCallLogic = true;
         m_bInline = true;
+
+        m_Elements = new ArrayList<>();
+        m_Placeholder = "";
+        m_Ending = ending;
     }
-    
+
+    protected FuncDeclNode_write Instantiate() {
+        return new FuncDeclNode_write();
+    }
+
     @Override
     public FuncDeclNode ValidateCall(FuncCallNode callNode) {
+        FuncDeclNode_write instance = Instantiate();
+        instance.SetParent(this);
+        StringBuilder builder = new StringBuilder();
+
         // Allows any amount of parameters, as long as they are primitive/can get serialized
         // Compare given parameters to primitive types
         for (AbstractSyntaxTree param : callNode.GetParameterList()) {
-            TypeNode CallParamType = param.GetType();
-            // if (!NamedTypeNode.IsPrimitiveType(CallParamType, false)) {
-            if (!(CallParamType instanceof PrimitiveTypeNode || PointerTypeNode.CharPointerNode.CompareType(CallParamType) || ArrayTypeNode.CharArrayNode.CompareType(CallParamType))) {
-                throw new TypeCheckException(this, "writeln only supports primitive types or charptr/chararray(strings)");
-            }
+            TypeNode callParamType = param.GetType();
+            builder.append(callParamType.GetTypePlaceholder());
 
-            // TODO: check for tostring method
-            //  signature: ([type]) -> char*
+            // Primitives and char* can get added direcly
+            if (callParamType instanceof PrimitiveTypeNode && !PrimitiveTypeNode.BoolNode.CompareType(callParamType)) {
+                instance.m_Elements.add(param);
+            } else if (PointerTypeNode.CharPointerNode.CompareType(callParamType)) {
+                instance.m_Elements.add(param);
+            } else {
+                // check for tostring method, signature: ([type]) -> char*
+                FuncCallNode toStringCall = new FuncCallNode(IO.TOSTRING);
+                toStringCall.AddParameter(param);
+                toStringCall.SetParent(instance);
+                toStringCall.CheckType();
+                instance.m_Elements.add(toStringCall);
+            }
         }
 
-        return this;
+        instance.m_Placeholder = builder.toString();
+        return instance;
     }
 
     @Override
@@ -46,42 +75,25 @@ public class FuncDeclNode_write extends FuncDeclNode_Core {
         // TODO: only one element with a single character? -> use "putchar"
         // TODO: add constants directly to the placeholder string, instead of adding a parameter
 
-        StringBuilder builder = new StringBuilder();
         List<ParamContainer> content = new ArrayList<>();
-
-        for (AbstractSyntaxTree element : callNode.GetParameterList()) {
+        for (AbstractSyntaxTree element : m_Elements) {
             ParamContainer elementContainer = element.CreateSnippet(slave);
+            elementContainer = AccessInterface.TryLoadValue(slave, element, elementContainer);
 
-            TypeNode elementType = element.GetType();
-            if (elementType instanceof ArrayTypeNode) {
-                // handle string array type
-                elementContainer = slave.CreateArrayElementPtr(elementContainer, 0);
-            } else {
-                // handle primitive types + string pointer type
-                elementContainer = AccessInterface.TryLoadValue(slave, element, elementContainer);
-
-                if (elementType.CompareType(PrimitiveTypeNode.BoolNode)) {
-                    elementContainer = slave.ExtendToInt(elementContainer);
-                }
-
-                // some primitive types require additional conversion
-                if (elementType instanceof PrimitiveTypeNode) {
-                    if (((PrimitiveTypeNode) elementType).GetTypeIsDezimal() && ((PrimitiveTypeNode) elementType).GetTypeSize() != 64) {
-                        // convert all decimals to double
-                        elementContainer = slave.ExtendFloatToDouble(elementContainer);
-                    }
+            // some primitive types require additional conversion
+            if (element.GetType() instanceof PrimitiveTypeNode) {
+                if (((PrimitiveTypeNode) element.GetType()).GetTypeIsDezimal() && ((PrimitiveTypeNode) element.GetType()).GetTypeSize() != 64) {
+                    // convert all decimals to double
+                    elementContainer = slave.ExtendFloatToDouble(elementContainer);
                 }
             }
 
             // Add parameter to function call
             content.add(elementContainer);
-
-            // Add placeholder element for parameter to string
-            builder.append(elementType.GetTypePlaceholder());
         }
 
-        //
-        ParamContainer constant = slave.CreateStringConstant(builder.toString());
+        // Add newline (must be added as hex (\0A instead of \n, counts as a single character))
+        ParamContainer constant = slave.CreateStringConstant(m_Placeholder + m_Ending);
         ParamContainer placeholderRef = slave.CreateArrayElementPtr(constant, new ValueWrapper_Constant("0"));
         slave.CreateNativeCall(new NativeFunction_printf(placeholderRef, content));
 
--- src/ast/core/functions/io/FuncDeclNode_writeln.java
+++ src/ast/core/functions/io/FuncDeclNode_writeln.java
@@ -16,100 +16,13 @@ import writer.wrappers.ValueWrapper_Constant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FuncDeclNode_writeln extends FuncDeclNode_Core {
-    private List<AbstractSyntaxTree> m_Elements;
-    private String m_Placeholder;
-
-    // TODO: maybe use puts instead of printf (no format required)
+public class FuncDeclNode_writeln extends FuncDeclNode_write {
     public FuncDeclNode_writeln() {
-        super(IO.WRITELN, VoidTypeNode.VoidNode);
-        m_bCustomCallLogic = true;
-        m_bInline = true;
-
-        m_Elements = new ArrayList<>();
-        m_Placeholder = "";
+        super(IO.WRITELN, "\n");
     }
 
     @Override
-    public FuncDeclNode ValidateCall(FuncCallNode callNode) {
-        FuncDeclNode_writeln instance = new FuncDeclNode_writeln();
-        instance.SetParent(this);
-        StringBuilder builder = new StringBuilder();
-
-        // Allows any amount of parameters, as long as they are primitive/can get serialized
-        // Compare given parameters to primitive types
-        for (AbstractSyntaxTree param : callNode.GetParameterList()) {
-            TypeNode callParamType = param.GetType();
-            builder.append(callParamType.GetTypePlaceholder());
-
-            if (callParamType instanceof PrimitiveTypeNode && !PrimitiveTypeNode.BoolNode.CompareType(callParamType)) {
-                instance.m_Elements.add(param);
-            } else if (PointerTypeNode.CharPointerNode.CompareType(callParamType)) {
-                instance.m_Elements.add(param);
-            } else {
-                FuncCallNode toStringCall = new FuncCallNode(IO.TOSTRING);
-                toStringCall.AddParameter(param);
-                toStringCall.SetParent(instance);
-                toStringCall.CheckType();
-                instance.m_Elements.add(toStringCall);
-            }
-
-            // TODO: check for tostring method
-            //  signature: ([type]) -> char*
-        }
-
-        instance.m_Placeholder = builder.toString();
-
-        return instance;
-    }
-
-    @Override
-    public ParamContainer CreateFunctionCall(GeneratorSlave slave, FuncCallNode callNode) {
-        // TODO: only one element with a single character? -> use "putchar"
-        // TODO: add constants directly to the placeholder string, instead of adding a parameter
-
-        StringBuilder builder = new StringBuilder();
-        List<ParamContainer> content = new ArrayList<>();
-
-        for (AbstractSyntaxTree element : m_Elements) {
-            ParamContainer elementContainer = element.CreateSnippet(slave);
-            elementContainer = AccessInterface.TryLoadValue(slave, element, elementContainer);
-
-            // TypeNode elementType = element.GetType();
-            // if (elementType instanceof ArrayTypeNode) {
-            //     // handle string array type
-            //     elementContainer = slave.CreateArrayElementPtr(elementContainer, 0);
-            // } else {
-            //     // handle primitive types + string pointer type
-            //     elementContainer = AccessInterface.TryLoadValue(slave, element, elementContainer);
-
-            //     if (elementType.CompareType(PrimitiveTypeNode.BoolNode)) {
-            //         elementContainer = slave.ExtendToInt(elementContainer);
-            //     }
-
-            //     // some primitive types require additional conversion
-            //     if (elementType instanceof PrimitiveTypeNode) {
-            //         if (((PrimitiveTypeNode) elementType).GetTypeIsDezimal() && ((PrimitiveTypeNode) elementType).GetTypeSize() != 64) {
-            //             // convert all decimals to double
-            //             elementContainer = slave.ExtendFloatToDouble(elementContainer);
-            //         }
-            //     }
-            // }
-
-            // Add parameter to function call
-            content.add(elementContainer);
-
-            // Add placeholder element for parameter to string
-            // builder.append(elementType.GetTypePlaceholder());
-        }
-        // Add newline (must be added as hex (\0A instead of \n, counts as a single character))
-        builder.append("\n");
-
-        //
-        ParamContainer constant = slave.CreateStringConstant(m_Placeholder + '\n');
-        ParamContainer placeholderRef = slave.CreateArrayElementPtr(constant, new ValueWrapper_Constant("0"));
-        slave.CreateNativeCall(new NativeFunction_printf(placeholderRef, content));
-
-        return null;
+    protected FuncDeclNode_write Instantiate() {
+        return new FuncDeclNode_writeln();
     }
 }
